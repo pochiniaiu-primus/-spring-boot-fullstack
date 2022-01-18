59dc125b53586228716336293951f51d15b7975a
Serhii Honchar <sehonc@softserveinc.com>
2022-01-18 20:07:27 +0200

    add Bootstrap

---
+++ src/main/java/com/serhiihonchar/fullstackspringbootwithreact/FullStackSpringBootWithReactApplication.java
@@ -0,0 +1,13 @@
+package com.serhiihonchar.fullstackspringbootwithreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackSpringBootWithReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackSpringBootWithReactApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/serhiihonchar/fullstackspringbootwithreact/student/Gender.java
@@ -0,0 +1,7 @@
+package com.serhiihonchar.fullstackspringbootwithreact.student;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
